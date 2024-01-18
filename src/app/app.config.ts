4cdbe318b1b1c0eb641818c2780587b4801d9e19
salilthegreat <salilkumardas22@gmail.com>
2024-01-18 22:27:38 +0530

    fire store connected to dashboard

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {provideFirebaseApp,initializeApp} from "@angular/fire/app"
+import {provideFirestore, getFirestore} from "@angular/fire/firestore"
+import {provideStorage, getStorage} from "@angular/fire/storage"
+import {environment} from "../environments/environment.development"
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(()=>initializeApp(environment.firebaseConfig)),
+      provideFirestore(()=>getFirestore()),
+      provideStorage(()=>getStorage())
+    )
+  ]
 };
